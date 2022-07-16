925de8e125e15043fd295d02c998b48baf81bf28
phivu99 <100842950+phivu99@users.noreply.github.com>
2022-07-16 14:36:19 +0700

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -146,36 +146,36 @@
     };
 
        // Header Fixed
-       var headerFixed = function () {
-        if ($('body').hasClass('header-fixed')) {
-            var nav = $('#header_main');
+    //    var headerFixed = function () {
+    //     if ($('body').hasClass('header-fixed')) {
+    //         var nav = $('#header_main');
 
-            if (nav.length) {
-                var offsetTop = nav.offset().top,
-                    headerHeight = nav.height(),
-                    injectSpace = $('<div />', {
-                        height: headerHeight
-                    }).insertAfter(nav);
-                injectSpace.hide();
+    //         if (nav.length) {
+    //             var offsetTop = nav.offset().top,
+    //                 headerHeight = nav.height(),
+    //                 injectSpace = $('<div />', {
+    //                     height: headerHeight
+    //                 }).insertAfter(nav);
+    //             injectSpace.hide();
 
-                $(window).on('load scroll', function () {
-                    if ($(window).scrollTop() > offsetTop) {
-                        nav.addClass('is-fixed');
-                        injectSpace.show();
-                    } else {
-                        nav.removeClass('is-fixed');
-                        injectSpace.hide();
-                    }
+    //             $(window).on('load scroll', function () {
+    //                 if ($(window).scrollTop() > offsetTop) {
+    //                     nav.addClass('is-fixed');
+    //                     injectSpace.show();
+    //                 } else {
+    //                     nav.removeClass('is-fixed');
+    //                     injectSpace.hide();
+    //                 }
 
-                    if ($(window).scrollTop() > 500) {
-                        nav.addClass('is-small');
-                    } else {
-                        nav.removeClass('is-small');
-                    }
-                })
-            }
-        }
-    };
+    //                 if ($(window).scrollTop() > 500) {
+    //                     nav.addClass('is-small');
+    //                 } else {
+    //                     nav.removeClass('is-small');
+    //                 }
+    //             })
+    //         }
+    //     }
+    // };
 
     // Mobile Navigation
     var mobileNav = function () {
